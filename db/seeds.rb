38f8cde4563cd5483fa5c65ec26f22e5a081d31d
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-13 15:11:04 -0600

    add functioning seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require 'csv'
+
+InvoiceItem.destroy_all
+Transaction.destroy_all
+Invoice.destroy_all
+Item.destroy_all
+Merchant.destroy_all
+Customer.destroy_all
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CSV.foreach("./db/data/merchants.csv", :headers => true) do |row|
+  Merchant.create!(row.to_hash)
+end
+puts "Merchants Seeded!"
+
+CSV.foreach("./db/data/customers.csv", :headers => true) do |row|
+  Customer.create!(row.to_hash)
+end
+puts "Customers Seeded!"
+
+CSV.foreach("./db/data/items.csv", :headers => true) do |row|
+  Item.create!(row.to_hash)
+end
+puts "Items Seeded!"
+
+CSV.foreach("./db/data/invoices.csv", :headers => true) do |row|
+  Invoice.create!(row.to_hash)
+end
+puts "Invoices Seeded!"
+
+CSV.foreach("./db/data/invoice_items.csv", :headers => true) do |row|
+  InvoiceItem.create!(row.to_hash)
+end
+puts "Invoice-Items Seeded!"
+
+CSV.foreach("./db/data/transactions.csv", headers: true, header_converters: :symbol) do |row|
+  Transaction.create!(invoice: Invoice.find(row[:invoice_id]),
+                      credit_card_number: row[:credit_card_number],
+                      result: row[:result],
+                      created_at: row[:transaction_created],
+                      updated_at: row[:transaction_updated],
+                      )
+end
+puts "Transactions Seeded!"
+
+puts "Seed complete!"
