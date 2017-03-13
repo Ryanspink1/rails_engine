5709ff6bec3030fa9649e2ad38ee73278d243963
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-13 16:27:30 -0600

    add shoulda matcher and model tests for merchant, transaction and customer.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  has_many :invoices
+  # has_many :transactions, through: :invoices
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  context "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  context "relationships" do
+    it { should have_many(:items) }
+    it { should have_many(:invoices) }
+  end
   
 end
