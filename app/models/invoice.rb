d427dc1229e387616f4391b7bba349b85d9a71eb
Ryan Spink <ryanspinktech@gmail.com>
2017-03-13 20:00:40 -0600

    adds model validation and relationship testing for items, invoices, and invoice_items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,7 @@
 class Invoice < ApplicationRecord
+  validates :customer_id, :merchant_id, :status, presence: true
   belongs_to :customer
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,5 @@
 class InvoiceItem < ApplicationRecord
+  validates :item_id, :invoice_id, :quantity, :unit_price, presence: true
   belongs_to :item
   belongs_to :invoice
 end
