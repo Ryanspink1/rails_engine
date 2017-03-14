6d75d17d25b1266f1b9c22b7099705d102d06384
Ryan Spink <ryanspinktech@gmail.com>
2017-03-13 20:52:02 -0600

    completes item requests and testing for item index and show

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "items API" do
+  it "sends a list of items" do
+    create_list(:merchant, 4)
+    create_list(:item, 4)
+
+    get '/api/v1/items'
+
+    expect(response).to be_success
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(4)
+  end
+
+  it "can get one item by its id" do
+    create_list(:merchant, 1)
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+
+    expect(item).to have_key "name"
+    expect(item).to have_key "description"
+    expect(item).to have_key "unit_price"
+    expect(item).to have_key "merchant_id"
+
+    expect(item["name"]).to eq("Moon Boot")
+    expect(item["description"]).to eq("Worn by Napoleon Dynamite")
+    expect(item["unit_price"]).to eq(1)
+    expect(item["unit_price"]).to_not eq(2)
+    expect(item["merchant_id"]).to eq(5)
+    expect(item["merchant_id"]).to_not eq(4)
+  end
+end
