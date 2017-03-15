680759687b0e969f6becf83a3e4dd3e5e0ed69f8
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-14 21:40:52 -0600

    adds stuff

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def total_revenue
+    
+  end
+
+
 end
