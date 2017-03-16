2d8da088ef27ea3dcf8dcd5c6e56048dbcc01bbd
Ryan Spink <ryanspinktech@gmail.com>
2017-03-16 11:47:18 -0600

    preliminary BIE AR completed, save Boss Mode

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -28,4 +28,15 @@ class Merchant < ApplicationRecord
       .limit(quantity)
   end
 
+  def favorite_customer
+    customers.joins(:invoices, :transactions).merge(Transaction.successful).select("customers.id, first_name, last_name, count(invoices.id) AS count_of_invoices").group("customers.id").order("count_of_invoices DESC").first
+  end
+
+  def self.most_items(quantity)
+    joins(invoices: [:invoice_items, :transactions]).merge(Transaction.successful).group('id').order("sum(invoice_items.quantity) DESC").limit(8)
+  end
+
+  def self.revenue(date)
+    .joins(:invoice_items).where(invoices: {created_at: date}).sum("invoice_items.quantity * invoice_items.unit_price")
+  end
 end
