d5bc52f4a2fbd0f6bafb9c68bf4c403017934740
Ryan Spink <ryanspinktech@gmail.com>
2017-03-15 19:37:22 -0600

    finishes best_day business endpoint for single item

--- spec/requests/api/v1/business_analytics/item_best_day_spec.rb
+++ spec/requests/api/v1/business_analytics/item_best_day_spec.rb
@@ -3,22 +3,21 @@ require 'rails_helper'
 describe "item was sold the most on" do
   it "this day" do
     create(:item, id:1)
-    create(:item, id:2)
-    create(:invoice_item, item_id:2, created_at: "2012-03-27T14:54:05.000Z")
-    create(:invoice_item, item_id:2, created_at: "2012-03-27T14:54:05.000Z")
-    create(:invoice_item, item_id:2, created_at: "2012-03-27T14:54:05.000Z")
-    create(:invoice_item, item_id:2, created_at: "2012-04-27T14:54:05.000Z")
-    create(:invoice_item, item_id:2, created_at: "2012-04-27T14:54:05.000Z")
-    # byebug
-    get '/api/v1/items/2/best_day'
+    create(:invoice, id:1, created_at: "2012-03-27T14:54:05.000Z")
+    create(:invoice_item, quantity:4, item_id:1, invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z")
+    create(:transaction, invoice_id: 1)
+    create(:invoice, id:2, created_at: "2012-04-27T14:54:05.000Z")
+    create(:invoice_item, quantity:3, item_id:1, invoice_id: 2, created_at: "2012-04-27T14:54:05.000Z")
+    create(:transaction, invoice_id: 2)
+
+    get '/api/v1/items/1/best_day'
 
     expect(response).to be_success
 
-    best_day = JSON.parse(response.body)[0]
+    best_day = JSON.parse(response.body)
 
-    expect(best_day.count).to eq(1)
-    expect(best_day[:created_at]).to eq("2012-03-27T14:54:05.000Z")
-    expect(best_day[:created_at]).to_not eq("2012-04-27T14:54:05.000Z")
+    expect(best_day["best_day"]).to eq("2012-03-27T14:54:05.000Z")
+    expect(best_day["best_day"]).to_not eq("2012-04-27T14:54:05.000Z")
 
   end
 end
